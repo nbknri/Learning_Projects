a5ad12686ea36ab21960221c624191f489f4dcc0
nbknri <nbknri@gmail.com>
2025-02-16 19:36:42 +0530

    Add reset button

--- Tic_Tac_Toe_Game/app.py
+++ Tic_Tac_Toe_Game/app.py
@@ -1,9 +1,12 @@
 from tkinter import *
 import random
+import time
 
-Window = Tk()
-Window.title("Tic Tac Toe Game")
-Window.geometry("400x400")
+window = Tk()
+window.title("Tic Tac Toe Game")
+window.geometry("400x400")
+frame = Frame(window, padx=20, pady=20)  # Adds margin inside the window
+frame.pack(padx=20, pady=20)
 
 # Board value store
 board = [" "]*10
@@ -80,6 +83,7 @@ def insert(letter,pos):
             pos = random.randint(1, 9)
             insert(letter,pos)
     
+    
     clm_1.config(text = board[1])
     clm_2.config(text = board[2])
     clm_3.config(text = board[3])
@@ -97,23 +101,40 @@ def human_move(letter,pos):
             computer_move(computer)
     
 
-def computer_move(letter):    
+def computer_move(letter):
     pos = random.randint(1, 9)
     insert(letter, pos)
 
 
-# Board settings
-clm_1 = Button(Window, text="1", width=6, height=2, padx=6, bd=3, command=lambda: human_move(human, 1))
-clm_2 = Button(Window, text="2", width=6, height=2, padx=6, bd=3, command=lambda: human_move(human, 2))
-clm_3 = Button(Window, text="3", width=6, height=2, padx=6, bd=3, command=lambda: human_move(human, 3))
-clm_4 = Button(Window, text="4", width=6, height=2, padx=6, bd=3, command=lambda: human_move(human, 4))
-clm_5 = Button(Window, text="5", width=6, height=2, padx=6, bd=3, command=lambda: human_move(human, 5))
-clm_6 = Button(Window, text="6", width=6, height=2, padx=6, bd=3, command=lambda: human_move(human, 6))
-clm_7 = Button(Window, text="7", width=6, height=2, padx=6, bd=3, command=lambda: human_move(human, 7))
-clm_8 = Button(Window, text="8", width=6, height=2, padx=6, bd=3, command=lambda: human_move(human, 8))
-clm_9 = Button(Window, text="9", width=6, height=2, padx=6, bd=3, command=lambda: human_move(human, 9))
+def reset_function():
+    global board
+    board = [" "]*10
+    clm_1.config(text = "")
+    clm_2.config(text = "")
+    clm_3.config(text = "")
+    clm_4.config(text = "")
+    clm_5.config(text = "")
+    clm_6.config(text = "")
+    clm_7.config(text = "")
+    clm_8.config(text = "")
+    clm_9.config(text = "")
+    result.config(text="")  
+    computer_move(computer)
 
-result = Label(Window, text="")
+
+# Board settings
+clm_1 = Button(frame, text="1", width=10, height=4, padx=6, bd=3, command=lambda: human_move(human, 1))
+clm_2 = Button(frame, text="2", width=10, height=4, padx=6, bd=3, command=lambda: human_move(human, 2))
+clm_3 = Button(frame, text="3", width=10, height=4, padx=6, bd=3, command=lambda: human_move(human, 3))
+clm_4 = Button(frame, text="4", width=10, height=4, padx=6, bd=3, command=lambda: human_move(human, 4))
+clm_5 = Button(frame, text="5", width=10, height=4, padx=6, bd=3, command=lambda: human_move(human, 5))
+clm_6 = Button(frame, text="6", width=10, height=4, padx=6, bd=3, command=lambda: human_move(human, 6))
+clm_7 = Button(frame, text="7", width=10, height=4, padx=6, bd=3, command=lambda: human_move(human, 7))
+clm_8 = Button(frame, text="8", width=10, height=4, padx=6, bd=3, command=lambda: human_move(human, 8))
+clm_9 = Button(frame, text="9", width=10, height=4, padx=6, bd=3, command=lambda: human_move(human, 9))
+
+result = Label(window, text="", height=3, padx=6)
+resetbtn = Button(window, text="Rest", width=10, height=3, bg="blue", fg="white", font=("Arial",10,"bold"), command=reset_function)
 
 # Board order settings
 clm_1.grid(row=0, column=0)
@@ -125,10 +146,11 @@ clm_6.grid(row=1, column=2)
 clm_7.grid(row=2, column=0)
 clm_8.grid(row=2, column=1)
 clm_9.grid(row=2, column=2)
-result.place(relx = 0.5, rely = 0.5, anchor = CENTER)
+result.place(relx = 0.5, rely = 0.75, anchor = CENTER)
+resetbtn.place(relx = 0.5, rely = 0.90, anchor = CENTER)
 
 
 computer_move(computer)
 
 
-Window.mainloop()
+window.mainloop()
