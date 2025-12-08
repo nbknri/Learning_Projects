58ddf95bb96a4551bbc14187b76660269e81d554
NBKNRI <nbknri@gamil.com>
2025-12-09 00:25:23 +0530

    feat: Refactor dependency injection for authentication components and streamline AuthBloc initialization

--- Flutter_Projects/blog_app/lib/init_dependencies.dart
+++ Flutter_Projects/blog_app/lib/init_dependencies.dart
@@ -1,5 +1,9 @@
 import 'package:blog_app/core/secrets/app_secrets.dart';
 import 'package:blog_app/features/auth/data/datasources/auth_remote_data_source.dart';
+import 'package:blog_app/features/auth/data/repository/auth_repository_impl.dart';
+import 'package:blog_app/features/auth/domain/repository/auth_repository.dart';
+import 'package:blog_app/features/auth/domain/usecases/user_sign_up.dart';
+import 'package:blog_app/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:get_it/get_it.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
@@ -15,7 +19,17 @@ Future<void> initDependencies() async {
 }
 
 void _initAuth() {
-  serviceLocator.registerFactory(
+  serviceLocator.registerFactory<AuthRemoteDataSource>(
     () => AuthRemoteDataSourceImpl(serviceLocator()),
+  );
+
+  serviceLocator.registerFactory<AuthRepository>(
+    () => AuthRepositoryImpl(serviceLocator()),
+  );
+
+  serviceLocator.registerFactory(() => UserSignUp(serviceLocator()));
+
+  serviceLocator.registerLazySingleton(
+    () => AuthBloc(userSignUp: serviceLocator()),
   );
 }
